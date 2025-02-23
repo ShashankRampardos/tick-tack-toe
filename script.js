d7b609a673a18375684cdd66ffb26d8b4b2508b2
Shashank Vashistha <sharampardo@gmail.com>
2025-02-23 19:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,23 +60,26 @@ window.addEventListener('DOMContentLoaded', () => {
         currentPlayer = 'O';
 
         // Try to win
-        let moveMade = tryToWinOrBlock('O');
+        let moveMade = tryToWinOrBlock('O');//if there is a straight winning move for computer then make that move
 
-        // Block player's win
+        // Block player's win, if player has a straight winning move then block that move
         if (!moveMade) {
             moveMade = tryToWinOrBlock('X');
         }
 
+
+        // first two conditions will give priority to center then corners, and if both are not available then make a random move
         // Take center if available
         if (!moveMade && board[4] === '') {
             makeMove(4);
             moveMade = true;
         }
 
+        // not using corner strategy,instead of making it challenging, it is making game too easy for player once he figure out the plan of logic.
         // Take a random corner
-        if (!moveMade) {
-            moveMade = takeRandomCell([0, 2, 6, 8]);
-        }
+        // if (!moveMade) {
+        //     moveMade = takeRandomCell([0, 2, 6, 8]);
+        // }
 
         // Take any available cell
         if (!moveMade) {
@@ -95,7 +98,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function tryToWinOrBlock(player) {
+    function tryToWinOrBlock(player) {// it is a modular function with the help of which we can check if there is a straight winning move for both computer or player
         for (let i = 0; i < board.length; i++) {
             if (board[i] === '') {
                 board[i] = player;
